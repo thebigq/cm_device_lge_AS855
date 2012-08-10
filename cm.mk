fb16545b214f6a570b67766ce4e19d9f0b6777ab
Tom Marshall <tdm.code@gmail.com>
2012-08-10 14:27:42 -0500

    Initial commit.

---
+++ cm.mk
@@ -0,0 +1,17 @@
+# Release name
+PRODUCT_RELEASE_NAME := Marquee
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/cdma.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lge/AS855/full_AS855.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := AS855
+PRODUCT_NAME := cm_AS855
+PRODUCT_BRAND := Sprint
+PRODUCT_MODEL := LG-AS855
+PROUDCT_MANUFACTURER := LGE
+PRODUCT_CHARACTERISTICS := phone
---
+++ device.mk
@@ -0,0 +1,19 @@
+$(call inherit-product-if-exists, vendor/lge/AS855/AS855-vendor.mk)
+
+include device/lge/sniper/device.mk
+
+DEVICE_PACKAGE_OVERLAYS += device/lge/AS855/overlay
+
+# XXX: Don't know what to do about these...
+CDMA_GOOGLE_BASE := android-lge-us
+CDMA_CARRIER_ALPHA := Sprint
+CDMA_CARRIER_NUMERIC := 310120
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.clientidbase.ms=$(CDMA_GOOGLE_BASE) \
+    ro.com.google.clientidbase.am=$(CDMA_GOOGLE_BASE) \
+    ro.com.google.clientidbase.yt=$(CDMA_GOOGLE_BASE) \
+    ro.cdma.home.operator.alpha=$(CDMA_CARRIER_ALPHA) \
+    ro.cdma.home.operator.numeric=$(CDMA_CARRIER_NUMERIC) \
+    gsm.operator.alpha=$(CDMA_CARRIER_ALPHA) \
+    gsm.operator.numeric=$(CDMA_CARRIER_NUMERIC)
