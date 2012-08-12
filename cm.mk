7cde9b14f30321d82907daa7fd5c20c7c334626f
Tom Marshall <tdm.code@gmail.com>
2012-08-12 16:29:34 -0500

    Fix PRODUCT_RELEASE_NAME and PRODUCT_BRAND.
    Values provided by Craptorman at AndroidForums.

--- cm.mk
+++ cm.mk
@@ -1,5 +1,5 @@
 # Release name
-PRODUCT_RELEASE_NAME := Marquee
+PRODUCT_RELEASE_NAME := Ignite
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
@@ -11,7 +11,7 @@ $(call inherit-product, device/lge/AS855/full_AS855.mk)
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := AS855
 PRODUCT_NAME := cm_AS855
-PRODUCT_BRAND := Sprint
+PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-AS855
 PROUDCT_MANUFACTURER := LGE
 PRODUCT_CHARACTERISTICS := phone
